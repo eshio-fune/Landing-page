48c35887d02d30f14ad2b2223feffd06eecf7167
eshio-fune <micglo77@gmail.com>
2026-02-02 21:02:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Select the enroll button
+const enrollButton = document.getElementById("enrollBtn");
 
+// Add click event
+enrollButton.addEventListener("click", function () {
+    alert("Thank you for your interest in NexaDev Academy! Enrollment opens soon 🚀");
+});
